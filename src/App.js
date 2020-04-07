ecb50d8c699d39eda0a752916df7027f9128ac7b
Carlos Gabriel <gabrielantero2012@hotmail.com>
2020-04-07 15:09:57 -0300

    All tests passed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    axios.get('/repositories').then(repository => setRepositories(repository.data));
+
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+
+   const repository = await axios.post('/repositories', {
+      title: `Novo item ${Date.now()}`,
+      url: 'https://github.com/gabriel-antero',
+      techs: 'ReactJS, React Native e NodeJS',
+    })
+    
+    setRepositories([...repositories, repository.data])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    await axios.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter((repository) => repository.id !== id));
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+             </button>
+          </li>
+        ))}
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
